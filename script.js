03a5c70cef1a6e7e538f6694a19cbb2da8a09455
stephennwachukwu <stephennwac007@gmail.com>
2024-06-29 15:00:54 +0100

    HNG Intro program

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const button = document.getElementById('change-text');
-
-button.addEventListener('click', () => {
-  const paragraph = document.querySelector('p');
-  paragraph.textContent = 'Thanks for visiting my website!';
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document loaded and ready');
 });
 
